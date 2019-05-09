626181167d2289ac1f8624cdfb02967235121bd4
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-05-09 02:35:09 -0300

    week 6 - finishing octave

--- week6/octave/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ week6/octave/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,16 +19,11 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+J = sum(((X * theta) - y) .^ 2) / (2*m) + lambda/(2*m) * sum(theta(2:end) .^ 2);
 
+grad = (((X * theta) - y)' * X) ./ m;
 
-
-
-
-
-
-
-
-
+grad(2:end) = grad(2:end) + (theta(2:end)' .* (lambda/m));
 
 % =========================================================================
 
